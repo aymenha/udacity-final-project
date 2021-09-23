edb074f584a174f158fd642b38aa50c4cd5901d9
Katja <katja.runge@jimdo.com>
2021-09-23 21:54:10 +0200

    feat: clone weather directory

---
+++ server.js
@@ -0,0 +1,45 @@
+let projectData = {};
+
+// Require Express to run server and routes
+const express = require("express");
+
+// Start up an instance of app
+const app = express();
+
+/* Middleware*/
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// // Cors for cross origin allowance
+const cors = require("cors");
+app.use(cors());
+
+// // Initialize the main project folder
+app.use(express.static("website"));
+
+const port = 3000;
+
+// // Setup Server
+
+const server = app.listen(port, listening);
+function listening() {
+  console.log(`running on localhost: ${port}`);
+}
+
+// GET route
+app.get("/get", sendData);
+function sendData(req, res) {
+  res.send(projectData);
+}
+
+// POST route
+app.post("/post", addWeather);
+function addWeather(req, res) {
+  projectData = {
+    city: req.body.city,
+    temperature: req.body.temperature,
+    feelings: req.body.feelings,
+    date: req.body.date,
+  };
+  res.send(projectData);
+}
---
+++ website/app.js
@@ -0,0 +1,67 @@
+const baseURL = "https://api.openweathermap.org/data/2.5/weather?zip=";
+const apiKey = "&appid=122ce078cd71684983156bf14ff9424b&units=metric";
+const zip = document.getElementById("inputZip");
+const country = document.getElementById("country");
+
+let d = new Date();
+let date = d.getMonth() + 1 + "." + d.getDate() + "." + d.getFullYear();
+
+document.getElementById("generate").addEventListener("click", performAction);
+
+function performAction(e) {
+  const feelings = document.getElementById("feelings").value;
+  getWeather(baseURL, zip.value + ",", country.value, apiKey).then(function (
+    data
+  ) {
+    postData("/post", {
+      city: data.name,
+      temperature: data.main.temp,
+      feelings: feelings,
+      date,
+    });
+    newEntryData();
+  });
+}
+
+const getWeather = async (url, zip, country, key) => {
+  const res = await fetch(url + zip + country + key);
+  try {
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
+const postData = async (url = "", data = {}) => {
+  console.log(data);
+  const response = await fetch(url, {
+    method: "POST",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+
+  try {
+    const newData = await response.json();
+    return newData;
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
+// create new entry with new data
+const newEntryData = async () => {
+  const request = await fetch("/get");
+  try {
+    const allData = await request.json();
+    document.getElementById("city").innerHTML = allData.city;
+    document.getElementById("temp").innerHTML = allData.temperature + " C°";
+    document.getElementById("feels").innerHTML = allData.feelings;
+    document.getElementById("date").innerHTML = allData.date;
+  } catch (error) {
+    console.log("error", error);
+  }
+};
