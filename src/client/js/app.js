724d37be4f78e90e8f4cdc4f0ea311d3215be227
Amine <mouhamedaymenhammami@gmail.com>
2021-10-20 23:45:59 +0200

    fix

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,2 +1,4 @@
-import { getCityName } from "./city";
+import { getCityName } from "./city.js";
 document.getElementById("generate").addEventListener("click", getCityName);
+
+export { getCityName };
