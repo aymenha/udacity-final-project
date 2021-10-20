0e3a7825705ca3237648b67794c48d5fddcdc552
Amine <mouhamedaymenhammami@gmail.com>
2021-10-20 22:09:17 +0200

    update test

--- __test__/app.spec.js
+++
@@ -1,5 +0,0 @@
-import { getCityName } from '../src/client/js/app';
-
-test('getCityName() must be defined', () => {
-  expect(getCityName).toBeDefined();
-});
---
+++ __test__/city.spec.js
@@ -0,0 +1,5 @@
+import { getCityName } from "../src/client/js/city";
+
+test("getCityName() must be defined", () => {
+  expect(getCityName).toBeDefined();
+});
